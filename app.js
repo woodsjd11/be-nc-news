436c89ae1cc73a58aa15ca54c25d23d367272eea
woodsjd11 <woodsjd@hotmail.co.uk>
2022-07-04 12:05:36 +0100

    3-GET/apic/topics pass test for happy path and invalid path

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const { getTopics } = require("./controllers");
 const app = express();
 
+app.get("/api/topics", getTopics);
+app.use('*', (req, res) =>{
+    res.status(404).send({message: "404 Error: Invalid Path"})
+})
 module.exports = app;
