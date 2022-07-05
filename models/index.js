858dce91cb3f744871564a8fac982effbf2c46b5
woodsjd11 <woodsjd@hotmail.co.uk>
2022-07-05 12:13:55 +0100

    add passing test for article with no comments

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ exports.fetchTopics = () => {
 exports.fetchArticleById = (article_id) => {
   return db
     .query(
-      "SELECT articles.*, COUNT(comments.article_id) AS comment_count FROM articles JOIN comments ON comments.article_id = articles.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id",
+      "SELECT articles.*, COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id",
       [article_id]
     )
     .then((data) => {
