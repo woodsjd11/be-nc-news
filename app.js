670989c97b529799114cd0885153375d77c6a3d7
woodsjd11 <woodsjd@hotmail.co.uk>
2022-07-06 12:53:22 +0100

    add 500 server error

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const {
   getCommentsByArticleId,
   getArticles,
   postCommentByArticleId,
-
 } = require("./controllers");
 
 const {
   handleCustomErrors,
   handlePsqlErrors,
+  serverError,
 } = require("./errors/error_handling");
 
 const app = express();
@@ -26,11 +26,9 @@ app.get("/api/articles/:article_id", getArticleById);
 
 app.get("/api/users", getUsers);
 
-
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 
 app.get("/api/articles", getArticles);
-
 
 app.patch("/api/articles/:article_id", patchArticleById);
 
@@ -42,4 +40,5 @@ app.use("*", (req, res) => {
 
 app.use(handleCustomErrors);
 app.use(handlePsqlErrors);
+app.use(serverError);
 module.exports = app;
