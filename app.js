965a833289c6e451fa2b54e6ce6e1ed09db74ae9
woodsjd11 <woodsjd@hotmail.co.uk>
2022-07-07 16:44:30 +0100

    add api-router file

--- app.js
+++ app.js
@@ -24,19 +24,21 @@ const {
 
 const app = express();
 
+const apiRouter = require('./routes/api-router')
+
 app.use(express.json());
 
-app.get("/api", getEndpoints);
+// app.get("/api", getEndpoints);
 
-app.get("/api/topics", getTopics);
+// app.get("/api/topics", getTopics);
 
-app.get("/api/users", getUsers);
+// app.get("/api/users", getUsers);
 
-app.get("/api/articles", getArticles);
+// app.get("/api/articles", getArticles);
 
-app.get("/api/articles/:article_id", getArticleById);
+// app.get("/api/articles/:article_id", getArticleById);
 
-app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
+// app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 
 app.patch("/api/articles/:article_id", patchArticleById);
 
