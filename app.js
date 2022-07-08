3d1c066ae9f1d35e5637c68b6f5ccf51acffa14f
woodsjd11 <woodsjd@hotmail.co.uk>
2022-07-08 13:12:21 +0100

    clean up routers

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const app = express();
 
 const {
   handleCustomErrors,
@@ -6,20 +7,11 @@ const {
   serverError,
 } = require("./errors/error_handling");
 
-const app = express();
-
 const apiRouter = require("./routes/api-router");
-const articleRouter = require("./routes/articles-router");
-const topicRouter = require("./routes/topics-router");
-const commentRouter = require("./routes/comments-router");
-const userRouter = require("./routes/users-router");
+
 app.use(express.json());
 
 app.use("/api", apiRouter);
-app.use("/api/articles", articleRouter);
-app.use("/api/topics", topicRouter);
-app.use("/api/comments", commentRouter);
-app.use("/api/users", userRouter);
 
 app.use("*", (req, res) => {
   res.status(404).send({ message: "404 Error: Invalid Path" });
--- routes/api-router.js
+++ routes/api-router.js
@@ -1,6 +1,14 @@
 const apiRouter = require("express").Router();
 const { getEndpoints } = require("../controllers/api-controller");
+const articleRouter = require("./articles-router");
+const userRouter = require("./users-router");
+const commentRouter = require("./comments-router");
+const topicRouter = require("./topics-router");
 
 apiRouter.get("/", getEndpoints);
+apiRouter.use("/articles", articleRouter);
+apiRouter.use("/topics", topicRouter);
+apiRouter.use("/comments", commentRouter);
+apiRouter.use("/users", userRouter);
 
 module.exports = apiRouter;
